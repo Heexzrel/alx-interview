515497babe7199f8c1129a0a590a17a7884cec42
George Israel <georgeisrael18@outlook.com>
2024-03-14 05:09:54 -0700

    keys

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Module that containes "canUnlockAll" function"""
+
+
+def canUnlockAll(boxes):
+    """
+        Return True if all boxes can be opened, else return False
+    """
+    if (type(boxes) is not list):
+        return False
+
+    if (len(boxes) == 0):
+        return False
+    keysbox = [0]
+    for key in keysbox:
+        for j in boxes[key]:
+            if j not in keysbox and j < len(boxes):
+                keysbox.append(j)
+    for i in range(len(boxes)):
+        if i not in keysbox:
+            return False
+    return True
